07be2aaa0dcb7404bdf6fddce104c39ba69d0ccd
tristo7 <tristo7000@gmail.com>
2016-10-01 12:11:59 -0500

    initial

---
+++ IO.php
@@ -0,0 +1,38 @@
+<?php
+	$servername = 	"localhost";
+	$username 	= 	"root";
+	$password 	= 	"G@m3c0ck$01";
+	$database 	=	"emergencyarea";
+	$conn = new mysqli($servername, $username, $password, $database);
+	// Verify connection to database
+	if (!$conn) {
+		die("Connection failed: " . mysqli_error($conn));
+	} // else we are connected.
+	
+	function save($coords, $expiration, $description){
+		global $conn;
+		$sql = "INSERT INTO data (latLngArray, expiration, description)
+			VALUES('".$coords."','".$expiration."','".$description."');";
+			
+		mysqli_query($conn,$sql);
+	}
+	
+	function load(){
+		global $conn;
+		$sql = "SELECT latLngArray, expiration, description FROM data WHERE expiration > NOW();";
+		$result = mysqli_query($conn,$sql);
+		$to_encode = array();
+		while($row = $result->fetch_assoc()) {
+		  $to_encode[] = $row;
+		}
+		echo json_encode($to_encode);
+	}
+	if(isset($_POST['latlng'])){
+		save($_POST['latlng'], $_POST['expires'], $_POST['desc']);
+	} else {
+		load();
+	}
+	
+	
+	mysqli_close($conn);
+?>
---
+++ README.md
@@ -0,0 +1,2 @@
+# emergency-map
+[View the map here.](http://tristo7.asuscomm.com/emergency-map/emergencyarea.html)
